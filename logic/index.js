f8acb09995b23b06396a59b6cad81dd088557024
patriagani <patria.gani@gmail.com>
2018-12-06 10:00:27 +0700

    logic sampai release 1

--- logic/index.js
+++ logic/index.js
@@ -1,6 +1,81 @@
 function generateChart(data) {
-  //your code here
+  // Bikin Board
+  let board = []
+  let maxStock = 0
+  let maxDay = 0
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].stock > maxStock) {
+      maxStock = data[i].stock
+    }
+    if (data[i].day > maxDay) {
+      maxDay = data[i].day
+    }
+  }
+  maxStock = maxStock + 2
+  maxDay = maxDay + 2
+  count = maxStock
+
+  for (let i = 0; i <= maxStock; i++) {
+    let temp = []
+    for (let j = 0; j <= maxDay; j++) {
+      if (i === maxStock) {
+        if (j === 0) {
+          temp.push('X')
+        }
+        else {
+          temp.push(String(j))
+        }
+      }
+      else {
+        if (j === 0) {
+          temp.push(count)
+          count--
+        }
+        else {
+          temp.push([])
+        }
+      }
+    }
+    board.push(temp)
+  }
+
+  //Masukin data (release 0)
+  let placedData = []
+  for (let i = 0; i < data.length; i++) {
+    let row = maxStock - data[i].stock
+    let col = data[i].day
+    board[row][col] = "#"
+    placedData.push([row, col])
+
+  }
+
+  console.log(placedData);
+
+  //Eksekusi
+  for (let i = 0; i < placedData.length; i++) {
+    if (i !== placedData.length-1) {
+      let data1 =  placedData[i]
+      let data2 = placedData[i+1]
+
+      //Down
+      if (data1[0] < data2[0]) {
+        let selisih = data2[0] - data1[0]
+        for (let j = 1; j <= selisih; j++) {
+          board[data1[0]+j][data1[1]] = "D"
+        }
+        let selisihHari = data2[1] - data1[1]
+      }
+      else if (data1[0] > data2[0]) {
+        let selisih = data1[0] - data2[0]
+        for (let j = 1; j <= selisih; j++) {
+          board[data1[0]-j][data1[1]] = "U"
+        }
+      }
+    }
+  }
+  return console.log(board);
 }
+
 
 const stockData1 = [{
   stock: 4,
@@ -46,7 +121,7 @@ const stockData2 = [{
   stock: 1,
   day: 5
 }]
-generateChart(stockData2)
+// generateChart(stockData2)
 
 /*
   [
