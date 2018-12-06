20032ebf2fd9c0983a6657b3903d785c977a4261
ahmad <ahmad.syukron6991@gmail.com>
2018-12-06 10:00:54 +0700

    first 45

--- logic/index.js
+++ logic/index.js
@@ -1,5 +1,44 @@
+
+function makeBoard (x,y) {
+  let board = []
+  for(let i = 1 ; i < x+2; i++){
+    let holder = []
+    for(let j = 1; j < y+2; j++){
+      if(j == 1) {
+        holder.push(String(y - i))
+      }
+      else if(i == x+1 && j !== 1) {
+        holder.push(String(j-1))
+      }
+      else {
+        holder.push(' ')
+      }
+    }
+    board.push(holder)
+  }
+  //console.log(board)
+  return board
+}
+
+
 function generateChart(data) {
-  //your code here
+  let stockY = 0
+  let dayX = 0
+  for(let i = 0; i < data.length; i++) {
+    if(data[i].stock > stockY) {
+      stockY = data[i].stock
+    }
+    if(data[i].day > dayX) {
+      dayX =  data[i].day
+    }
+  }
+  let length = []
+  length[0] = stockY+2
+  length[1] = dayX+2
+  console.log(length)
+  console.log(makeBoard(length[0],length[1]))
+  
+
 }
 
 const stockData1 = [{
